9314b4bd109889729d2098de7a198d51f0a8b1df
muratmnz <minazzmurat@gmail.com>
2021-02-04 04:25:40 +0300

    Initial commit

---
+++ FormManagementProject.Business/Controllers/FormController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using FormManagementProject.DataAccess.Models;
+
+namespace FormManagementProject.Business.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+       
+        public ActionResult List(string name)
+        {
+            using (FormDbEntities db = new FormDbEntities())
+            {
+                var values = from d in db.Form select d;
+                if (!string.IsNullOrEmpty(name))
+                {
+                    values = values.Where(m => m.name.Contains(name));
+                }
+                return View(values.ToList());
+            }
+
+        }
+        
+        public ActionResult AddForm()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult AddForm(Form form)
+        {
+            using (FormDbEntities db = new FormDbEntities())
+            {
+                UsersEntities userDb = new UsersEntities();
+
+                Form newForm = new Form();
+
+                int userId = userDb.Users.FirstOrDefault(x => x.username == System.Web.HttpContext.Current.User.Identity.Name).Id;
+
+                if (form.name != null && form.formUserName != null && form.formUserSurname != null )
+                {
+                    newForm.Id = form.Id;
+                    newForm.name = form.name;
+                    newForm.description = form.description;
+                    newForm.createdAt = DateTime.Now;
+                    newForm.createdBy = Convert.ToInt32(userId);
+                    newForm.fields = form.fields;
+                    newForm.formUserName = form.formUserName;
+                    newForm.formUserSurname = form.formUserSurname;
+                    newForm.formUserAge = form.formUserAge;
+                    db.Form.Add(newForm);
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+                else
+                {                  
+                    return RedirectToAction("List");
+                }
+                    
+
+                
+            }
+
+        }
+
+        [Route("Form/{id:int}")]
+        public ActionResult Details(int id)
+        {
+           using(FormDbEntities db = new FormDbEntities()) { 
+            Form detailForm = db.Form.Find(id);
+
+            return View(detailForm);
+            }
+        }
+        [HttpPost]
+        public ActionResult Details(Form form)
+        {
+            using(FormDbEntities db = new FormDbEntities()) 
+            {
+
+                Form detailForm = db.Form.Find(form.Id);
+                detailForm.createdAt = form.createdAt;
+                detailForm.name = form.name;
+                detailForm.description = form.description;
+                detailForm.formUserName = form.formUserName;
+                detailForm.formUserSurname = form.formUserSurname;
+                detailForm.formUserAge = form.formUserAge;
+                
+                return RedirectToAction("List");
+            }
+        }
+
+    }
+}
---
+++ FormManagementProject.Business/Controllers/SecurityController.cs
@@ -0,0 +1,44 @@
+﻿using FormManagementProject.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormManagementProject.Business.Controllers
+{
+    [AllowAnonymous]
+    public class SecurityController : Controller
+    {
+        UsersEntities db = new UsersEntities();
+
+        // GET: Security
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users user)
+        {
+            var userInDb = db.Users.FirstOrDefault(x => x.username == user.username && x.password == user.password);
+
+            if (userInDb != null)
+            {
+                FormsAuthentication.SetAuthCookie(user.username, false);
+                return RedirectToAction("List", "Form");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid user. Username or Password is wrong";
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
